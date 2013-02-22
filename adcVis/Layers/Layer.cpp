d2e6bba804a54d2537b9a1b1eea0eb56613b6070
atristandyer <bigblob202@gmail.com>
2013-02-22 16:12:03 -0500

    Worked more on Layer

--- adcVis/Layers/Layer.cpp
+++ adcVis/Layers/Layer.cpp
@@ -10,23 +10,49 @@ Layer::Layer()
 	layerCount++;
 	layerID = layerCount;
 
+	minX = 0.0;
+	maxX = 0.0;
+	minY = 0.0;
+	maxY = 0.0;
 	minZ = 0.0;
 	maxZ = 0.0;
+
+	vaoID = 0;
+	vboID = 0;
+	iboID = 0;
+	outlineShader = 0;
+	fillShader = 0;
+	offsetValue = 0;
 }
 
 
+/**
+ * @brief Layer::Draw This function is used to draw the layer in the OpenGL context.
+ *
+ * This function first checks that data has been loaded to the OpenGL context and that
+ * both shaders have been set before binding the Layer's vertex array object and
+ * drawing the Layer.
+ *
+ */
 void Layer::Draw()
 {
 
 }
 
 
+/**
+ * @brief Layer::Update
+ */
 void Layer::Update()
 {
 
 }
 
 
+/**
+ * @brief Layer::Update
+ * @param value
+ */
 void Layer::Update(int value)
 {
 
@@ -78,23 +104,63 @@ float Layer::GetMaxZ()
 
 /**
  * @brief Layer::GetNode returns a pointer to the requested Node.
+ *
+ * This function is meant to be implemented by a subclass of Layer. Specifically,
+ * Layers that are modeling terrain should typically be able to return specific
+ * Nodes. Layers that are modeling water, on the other hand, will usually only
+ * contain a list of z-coordinates in order to keep memory footprint low, and
+ * therefore will not be able to return a Node.
+ *
+ * If it is not overridden, this function will always return 0.
+ *
  * @param nodeNumber The node number as defined in the fort.14 file.
  * @return A pointer to the Node object if the node number is valid.
  * @return 0 otherwise.
  */
 Node* Layer::GetNode(unsigned int nodeNumber)
 {
-
+	return 0;
 }
 
 
 /**
  * @brief Layer::GetElement returns a pointer to the requested Element.
+ *
+ * This function is meant to be implemented by a subclass of Layer. Specifically,
+ * Layers that are modeling terrain should typically be able to return specific
+ * Elements. Layers that are modeling water, on the other hand, will usually only
+ * contain a list of z-coordinates in order to keep memory footprint low, and
+ * therefore will not be able to return an Element.
+ *
+ * If it is not overridden, this function will always return 0.
+ *
  * @param elementNumber The element number as defined in the fort.14 file.
  * @return A pointer to the Element object if the element number is valid.
  * @return 0 otherwise.
  */
 Element* Layer::GetElement(unsigned int elementNumber)
+{
+	return 0;
+}
+
+
+/**
+ * @brief LoadDataToGPU Transfers node and element data to the OpenGL context.
+ *
+ * This function is used to pass Node and Element data to the OpenGL context in order to
+ * be rendered. Node location data is densely packed in order:
+ * - [x1, y1, z1, 1.0, x2, y2, z2, 1.0, ...]
+ * .
+ * Element data is also densely packed in order:
+ * - [e1n1, e1n2, e1n3, e2n1, e2n2, e2n3, ...]
+ * .
+ *
+ * We make use of Vertex Array Objects, so once the Node data is put into a Vertex
+ * Buffer Object and Element data is put into an Index Buffer Object, the OpenGL state
+ * is saved in the VAO state.
+ *
+ */
+void Layer::LoadDataToGPU()
 {
 
 }
--- adcVis/Layers/Layer.h
+++ adcVis/Layers/Layer.h
@@ -32,15 +32,19 @@ class Layer
 	protected:
 
 		// Generic Variables
-		std::vector<Node>	nodes;			/**< List of all Nodes in the Layer */
-		std::vector<Element>	elements;		/**< List of all Elements in the Layer */
-		unsigned int		numNodes;		/**< The number of Nodes in the Layer as specified in fort.14 */
-		unsigned int		numElements;		/**< The number of Elements in the Layer as specified in fort.14 */
-		float			minZ;			/**< The elevation of the lowest Node */
-		float			maxZ;			/**< The elevation of the highest Node */
+		std::vector<Node>	nodes;		/**< List of all Nodes in the Layer */
+		std::vector<Element>	elements;	/**< List of all Elements in the Layer */
+		unsigned int		numNodes;	/**< The number of Nodes in the Layer as specified in fort.14 */
+		unsigned int		numElements;	/**< The number of Elements in the Layer as specified in fort.14 */
+		float			minX;		/**< The minimum x-coordinate out of all Nodes */
+		float			maxX;		/**< The maximum x-coordinate out of all Nodes */
+		float			minY;		/**< The minimum y-coordinate out of all Nodes */
+		float			maxY;		/**< The maximum y-coordinate out of all Nodes */
+		float			minZ;		/**< The elevation of the lowest Node */
+		float			maxZ;		/**< The elevation of the highest Node */
 
 		// Flags
-		bool	glLoaded;
+		bool	glLoaded;			/**< Flag shows if the data has been loaded to the OpenGL context */
 
 		// OpenGL Variables
 		GLuint		vaoID;			/**< The vertex array object ID */
@@ -48,9 +52,10 @@ class Layer
 		GLuint		iboID;			/**< The index buffer object ID */
 		GLShader	*outlineShader;		/**< Pointer to the shader used for drawing outlines */
 		GLShader	*fillShader;		/**< Pointer to the shader used for drawing fill */
+		int		offsetValue;		/**< The value used to prevent z-fighting during draw operations */
 
 		// Protected Functions
-		virtual void	LoadDataToGPU() = 0;
+		void	LoadDataToGPU();
 
 
 	private:
